23749c1154529b7204a07737075563799a9d82b9
La Anh Quan <laanhquan1807@gmail.com>
2025-05-20 18:28:27 +0700

    Reuse Component Button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,13 @@
-import { StyleSheet, Text, View } from "react-native"
-
+import { Text, View, StyleSheet } from "react-native"
+import ShareButton from "components/button/share.button";
+import { APP_COLOR } from "utils/constant";
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         borderColor: "red",
-        borderWidth: 5
+        borderWidth: 5,
     },
     welcomeText: {
         flex: 0.6,
@@ -18,7 +20,8 @@ const styles = StyleSheet.create({
     welcomeBtn: {
         flex: 0.4,
         borderColor: "grey",
-        borderWidth: 5
+        borderWidth: 5,
+        gap: 20
     },
     heading: {
         fontSize: 40,
@@ -26,7 +29,7 @@ const styles = StyleSheet.create({
     },
     body: {
         fontSize: 30,
-        color: "orange",
+        color: APP_COLOR.ORANGE,
         marginVertical: 10,
     },
     footer: {
@@ -54,32 +57,84 @@ const WelcomePage = () => {
                     Welcome to
                 </Text>
                 <Text style={styles.body}>
-                    XNOTPRK - HK - Shop
+                    XNOTPRK - HK - ShopApp
                 </Text>
                 <Text style={styles.footer}>
-                    Buy every things If you want
+                    Buy every thing, If you want
                 </Text>
             </View>
 
             <View style={styles.welcomeBtn}>
-                <Text>
-                    Login with
-                </Text>
-                <View>
-                    <View style={styles.btnContainer}>
-                        <View style={styles.btnContent}>
-                            <Text style={styles.btnText}>Facebook</Text>
-                        </View>
-
-                    </View>
-                    <View><Text>Google</Text></View>
+                <View style={{
+                    borderBottomWidth: 1,
+                    borderBottomColor: "red",
+                    marginHorizontal: 50,
+                }}>
+                    <Text style={{
+                        padding: 10,
+                        textAlign: "center",
+                        backgroundColor: "white",
+                        alignSelf: "center",
+                        position: "relative",
+                        top: 20
+                    }}>
+                        Sign in with
+                    </Text>
                 </View>
-                <View><Text>Login with email or phone</Text></View>
-                <View><Text>Don't have an account? Sign up.</Text></View>
+                <View style={{
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    gap: 30
+                }}>
+                    <ShareButton
+                        title="faceBook"
+                        onPress={() => { alert("me") }}
+                        textStyle={{ textTransform: "uppercase" }}
+                        btnStyle={{
+                            justifyContent: "center",
+                            borderRadius: 30,
+                            backgroundColor: "#fff"
+                        }}
+                        icons={
+                            <FontAwesome5 name="facebook" size={30} color="black" />
+                        }
+                    />
+
+                    <ShareButton
+                        title="google"
+                        onPress={() => { alert("me") }}
+                        textStyle={{ textTransform: "uppercase" }}
+                        btnStyle={{
+                            justifyContent: "center",
+                            borderRadius: 30,
+                            paddingHorizontal: 20,
+                            backgroundColor: "#fff"
+                        }}
+                        icons={
+                            <FontAwesome5 name="google" size={30} color="black" />
+                        }
+                    />
+
+                </View>
+                <View>
+                    <ShareButton
+                        title="Start with your email or phone"
+                        onPress={() => { alert("me") }}
+                        textStyle={{ color: "#fff", paddingVertical: 5 }}
+                        btnStyle={{
+                            justifyContent: "center",
+                            borderRadius: 30,
+                            marginHorizontal: 50,
+                            paddingVertical: 10,
+                            backgroundColor: "#2c2c2c"
+                        }}
+                        pressStyle={{ alignSelf: "stretch" }}
+                    />
+                </View>
+                <View><Text style={{ textAlign: "center" }}>Already have an account? Sign In.</Text></View>
             </View>
         </View>
-
     )
 }
 
-export default WelcomePage
+export default WelcomePage;
---
+++ src/utils/constant.ts
@@ -0,0 +1,5 @@
+
+export const APP_COLOR = {
+    BLUE: "#1E90FF",
+    ORANGE: "#f4511e"
+}
