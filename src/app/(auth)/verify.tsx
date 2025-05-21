5cec2bf39a0a33243210f3b5b9e5a6ff50d33048
La Anh Quan <laanhquan1807@gmail.com>
2025-05-21 22:33:45 +0700

    Design Verify Code Screen

--- src/app/(auth)/verify.tsx
+++ src/app/(auth)/verify.tsx
@@ -1,9 +1,43 @@
-import { Text, View } from "react-native"
+import { APP_COLOR } from "@/utils/constant";
+import { View, Text, StyleSheet } from "react-native"
+import OTPTextView from 'react-native-otp-textinput';
+
+const styles = StyleSheet.create({
+    container: {
+        paddingVertical: 30,
+        paddingHorizontal: 20,
+    },
+    heading: {
+        fontSize: 25,
+        fontWeight: "600",
+        marginVertical: 20
+    }
+})
 
 const VerifyPage = () => {
     return (
-        <View>
-            <Text>verify page</Text>
+        <View style={styles.container}>
+            <Text style={styles.heading}>Verification Code</Text>
+            <Text style={{ marginVertical: 10 }}>Please type the verification code send to hoidanit@gmail.com</Text>
+            <View style={{ marginVertical: 20 }}>
+                <OTPTextView
+                    inputCount={6}
+                    inputCellLength={1}
+                    tintColor={APP_COLOR.ORANGE}
+                    textInputStyle={{
+                        borderWidth: 1,
+                        borderColor: APP_COLOR.GREY,
+                        borderBottomWidth: 1,
+                        borderRadius: 5,
+                        // @ts-ignore:next-line
+                        color: APP_COLOR.ORANGE
+                    }}
+                />
+            </View>
+            <View style={{ flexDirection: "row", marginVertical: 10 }}>
+                <Text >I don't receive a code!</Text>
+                <Text style={{ textDecorationLine: 'underline', color: APP_COLOR.ORANGE }}> Please resend</Text>
+            </View>
         </View>
     )
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,38 +1,44 @@
-import { Slot, Stack } from "expo-router"
-import { Text, View } from "react-native"
+import { Stack } from "expo-router"
 import { RootSiblingParent } from 'react-native-root-siblings';
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const RootLayout = () => {
     return (
         <RootSiblingParent>
-            <Stack
-                screenOptions={{
-                    headerStyle: {
-                        backgroundColor: '#f4511e',
-                    },
-                    headerTintColor: '#fff',
-                    headerTitleStyle: {
-                        fontWeight: 'bold',
-                    },
-                }}>
-                <Stack.Screen
-                    name="index"
-                    options={{ headerShown: false }}
-                />
-                <Stack.Screen
-                    name="(auth)/signup"
-                    options={{ headerShown: false }}
-                />
-                <Stack.Screen name="(tabs)"
-                    options={{ headerTitle: "Home" }}
-                />
-                <Stack.Screen name="product/index"
-                    options={{ headerTitle: "Product" }}
-                />
-                <Stack.Screen name="(auth)/login"
-                    options={{ headerTitle: "Login" }}
-                />
-            </Stack>
+            <SafeAreaView style={{ flex: 1 }}>
+                <Stack
+                    screenOptions={{
+                        headerStyle: {
+                            backgroundColor: '#f4511e',
+                        },
+                        headerTintColor: '#fff',
+                        headerTitleStyle: {
+                            fontWeight: 'bold',
+                        },
+                    }}>
+                    <Stack.Screen
+                        name="index"
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="(auth)/signup"
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="(auth)/verify"
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen name="(tabs)"
+                        options={{ headerTitle: "Home" }}
+                    />
+                    <Stack.Screen name="product/index"
+                        options={{ headerTitle: "Product" }}
+                    />
+                    <Stack.Screen name="(auth)/login"
+                        options={{ headerTitle: "Login" }}
+                    />
+                </Stack>
+            </SafeAreaView>
         </RootSiblingParent>
     )
 }
